1473407f8f074f65f5e6fa28de11755a7a1f3181
Benjamin Smith <bbsmith1107@email.campbell.edu>
2021-01-25 08:26:18 -0500

    index.html

--- routes/api.js
+++ routes/api.js
@@ -18,6 +18,8 @@ router.post("/api/transaction/bulk", ({body}, res) => {
       res.status(404).json(err);
     });
 });
+//essential part, user is constantly able to add/subtract funds
+//(user must refresh to start over again)
 router.get("/api/transaction", (req, res) => {
   Transaction.find({})
     .sort({ date: -1 })
